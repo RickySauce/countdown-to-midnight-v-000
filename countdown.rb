58b3fa9c627d086a33fc66544450ae0995597ee1
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-06 14:07:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(x)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  x = 10
+  until x == 0
+    puts "#{x} SECOND(S)!"
+    sleep.1
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
